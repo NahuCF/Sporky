094ad6f759782d485e5cb4ae13b4782d40df5f7f
Nahuel Fernandez <nahuefer105@outlook.com>
2023-05-11 18:23:22 -0300

    Added test dispatcher in Kernel

--- core/Http/Kernel.php
+++ core/Http/Kernel.php
@@ -8,6 +8,14 @@ class Kernel
 {
     public function handle(Request $request) : Response
     {
+        $dispatcher = new Dispatcher(function(RouteCollector $r) {
+            $r->addRoute('GET', '/', function() {
+                echo "Working";
+            });
+        });
+
+        $dispatcher->dispatch();
+
         return new Response('');
     }
 }
