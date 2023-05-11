9704927a9651bfe7f6fa5bf5843f63186783e4d5
Nahuel Fernandez <nahuefer105@outlook.com>
2023-05-10 21:31:07 -0300

    Insantiated kernel class in index

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
-<?php
+<?php declare(strict_types=1);
+
+use Sporky\Core\Http\Kernel;
+use Sporky\Core\Http\Request;
 
 require_once '../vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+
+$kernel = new Kernel;
+$response = $kernel->handle($request);
+$response->send();
